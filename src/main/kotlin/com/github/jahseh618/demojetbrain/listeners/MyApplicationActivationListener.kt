819c9dfe81dda532512d2df94135244cf6075924
GitHub Action <action@github.com>
2023-12-27 08:38:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/jahseh618/demojetbrain/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jahseh618/demojetbrain/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jahseh618.demojetbrain.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/jahseh618/demojetbrain/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/jahseh618/demojetbrain/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.jahseh618.demojetbrain.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jahseh618.demojetbrain.MyBundle
+import com.github.jahseh618.demojetbrain.services.MyProjectService
 import javax.swing.JButton
 
 
